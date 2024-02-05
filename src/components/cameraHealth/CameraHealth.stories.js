5f047d59cf55fec05ebed36be5f2a31b750d4fcb
Ayush <rishiustaad@gmail.com>
2024-02-05 17:37:01 +0530

    implemented cameraHealth component with storybook

---
+++ src/components/cameraHealth/CameraHealth.stories.js
@@ -0,0 +1,12 @@
+import CameraHealth from "./CameraHealth";
+
+export default {
+  component: CameraHealth,
+  title: "Camera Health",
+};
+
+export const Primary = {
+  args: {
+    cameras: "120",
+  },
+};
